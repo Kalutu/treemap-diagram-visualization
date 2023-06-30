957ac5e5377113fc1bbba9bb71bef606b2dce8f9
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-06-30 10:18:41 +0300

    modified:   script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let movieDataURL ="https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json";
 
+let movieData;
+
+let canvas = d3.select("#canvas");
+
+let drawTreeMap = () => {
+
+}
